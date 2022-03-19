5b4cb65c44984698db2e18f0b3dab9c0455f0b75
Alejandro Penacho <alejandro.per@hotmail.es>
2022-03-19 14:45:26 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+
+#[derive(Debug)]
+struct Question {
+    text: String,
+    answers: Vec<String>,
+    correct: usize
+}
+
+
+#[tokio::main]
+async fn main() {
+    let questions = read_file("questions/Q1.txt");
+    println!("{:?}", questions);
+}
+
+
+
+fn read_file(path: &str) -> Vec<Question> {
+    let file = std::fs::read_to_string(path).unwrap();
+    let mut lines = file.lines();
+
+    let mut out = Vec::new();
+
+    loop{
+
+        let mut question: Vec<&str> = lines.by_ref().take(6).collect();
+        if question.len() < 6 {return out};
+
+        let correct = question[1..5].iter()
+            .enumerate()
+            .find(|(_,ans)| ans.chars().last() == Some('*'))
+            .expect("Question with no correct answer").0;
+
+        question[correct+1] = 
+            &question[correct+1][0..(question[correct+1].len()-1)];
+
+        out.push(
+            Question{
+                text: question[0].to_owned(),
+                answers: question[1..5]
+                    .iter().map(|x| String::from(*x)).collect(),
+                correct 
+            }
+        );
+
+    }
+
+}
